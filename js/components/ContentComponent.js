1da3c16e310438a67f846f95e975eea268cfe3f4
Wido Do <widodo@saas-studios.com>
2024-12-03 19:11:46 +0700

    about

--- js/components/ContentComponent.js
+++ js/components/ContentComponent.js
@@ -1,25 +1,26 @@
 // PostComponent.js
 export const PostComponent = {
-  props: ['post'],
+  props: ["post"],
   template: `
     <div class="portfolio-item">
       <a href="#" @click.prevent="$emit('view-post', post.id)">
         {{ post.title }}
       </a>
     </div>
-  `
- };
- 
- // ContentComponent.js
- export default {
+  `,
+};
+
+// ContentComponent.js
+export default {
   props: ["currentView"],
   components: {
-    PostComponent
+    PostComponent,
   },
   data() {
     return {
       blogPosts: [],
       selectedPost: null,
+      aboutPage: null, // Menyimpan data halaman "About"
       apiKey: "AIzaSyC0NYs0vzrlklopzeDMW2mZvWTJ3z-y5iE",
       blogId: "2531488134356491737",
     };
@@ -30,24 +31,55 @@ export const PostComponent = {
       try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        this.blogPosts = data.items.map(post => ({
+        this.blogPosts = data.items.map((post) => ({
           id: post.id,
           title: post.title,
-          content: post.content
+          content: post.content,
         }));
       } catch (error) {
         console.error("Error fetching blog posts:", error);
       }
     },
+    async fetchAbout() {
+      const apiUrl = `https://www.googleapis.com/blogger/v3/blogs/${this.blogId}/pages/1121065789300202390?key=${this.apiKey}`;
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        // Menyimpan detail halaman "About"
+        this.aboutPage = {
+          id: data.id,
+          title: data.title,
+          content: data.content,
+        };
+
+        console.log("About page loaded:", this.aboutPage);
+      } catch (error) {
+        console.error("Error fetching about page:", error);
+      }
+    },
     async viewPost(postId) {
-      this.selectedPost = this.blogPosts.find(post => post.id === postId);
+      this.selectedPost = this.blogPosts.find((post) => post.id === postId);
     },
     backToBlogList() {
       this.selectedPost = null;
-    }
+    },
+  },
+  watch: {
+    currentView(newView) {
+      if (newView === "about" && !this.aboutPage) {
+        this.fetchAbout(); // Muat konten "About" saat view berubah ke "about"
+      }
+    },
   },
   created() {
     this.fetchBlogPosts();
+    if (this.currentView === "about") {
+      this.fetchAbout();
+    }
   },
   template: `
     <div class="container">
@@ -71,9 +103,9 @@ export const PostComponent = {
       </div>
  
       <div v-if="currentView === 'about'">
-      <div class="about">
-        <p>Hi! I'm a web developer passionate about creating amazing experiences on the web.</p>
-      </div>
+        <div class="about">
+          <div v-html="aboutPage?.content || '<p>Loading content...</p>'"></div>
+        </div>
       </div>
  
       <div v-if="currentView === 'portfolio'">
@@ -81,11 +113,8 @@ export const PostComponent = {
           <h4>Project A</h4>
           <p>Description of Project A</p>
         </div>
-        <div class="portfolio-item">
-          <h2>Project B</h2>
-          <p>Description of Project B</p>
-        </div>
       </div>
     </div>
-  `
- };
+  `,
+};
+
